155a4cfdffc8857526cafd28338f7e12cf684317
yashika <yashika0432@gmail.com>
2026-01-01 22:25:13 +0530

    starting static work sec

--- frontend/src/pages/work.jsx
+++ frontend/src/pages/work.jsx
@@ -1,5 +1,17 @@
+import "../style/work.css";
+
 function Work() {
-  return <></>;
+  return (
+    <div>
+      <container className="worksection">
+        <div className="access">
+          <img src="/work.png" className="leftimg" alt=""></img>
+          <img src="/tool.png" className="rightimg" alt=""></img>
+        </div>
+        <div className="mainsection"></div>
+      </container>
+    </div>
+  );
 }
 
 export default Work;
