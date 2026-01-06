40bda6dc3011917d57f5d47df5b8e8c5b4e737bf
yashika <yashika0432@gmail.com>
2026-01-06 20:37:34 +0530

    poster work main work

--- frontend/src/pages/posters.jsx
+++ frontend/src/pages/posters.jsx
@@ -9,15 +9,29 @@ function PosterWork() {
       </div>
       <div className="workspacePoster">
         <img src="/dialog.png" className="parts iconDialog" alt="" />
-        <img src="/headUp.png" className="photo1 parts" alt=""></img>
+        <div className="splitWrapper" alt="">
+          <img src="/headUp.png" className="photo1 imgsplit" alt=""></img>
+
+          {/* <div className="posts">
+            <img src="/f1.png" alt=""></img>
+            <img src="/thumbnail.png" alt=""></img>
+            <img src="/thumbnail2.png" alt=""></img>
+            <img src="/f1cards.png" alt=""></img>
+            <img src="/f1Poster.png" alt=""></img>
+            <img src="/thumbnail3.png" alt=""></img>
+            <img src="/ad.png" alt=""></img>
+            <img src="/ad2.png" alt=""></img>
+          </div> */}
+
+          <img src="/headDown.png" className="photo2 imgsplit" alt=""></img>
+        </div>
         <img src="/hovertext.png" className="parts hoverhere" alt="" />
         <img src="/box.png" className="parts boxes" alt="" />
         <img src="/pen.png" className="parts pen" alt="" />
-        <img
-          src="/functionsRight.png"
-          className="parts workRightBlock"
-          alt=""
-        />
+        <div className="properties-panel">
+          <img src="/functionsRight.png" alt="" />
+        </div>
+
         <div className="mainpart"></div>
       </div>
     </div>
