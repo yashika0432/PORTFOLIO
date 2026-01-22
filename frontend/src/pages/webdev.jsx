435fd26484b4ec7105a7c3de496d6a6fd480aa03
yashika <yashika0432@gmail.com>
2026-01-22 20:02:52 +0530

    start webdev

--- frontend/src/pages/webdev.jsx
+++ frontend/src/pages/webdev.jsx
@@ -1,7 +1,14 @@
 import "../style/webdev.css";
 
 function Webdevelopment() {
-  return <>website</>;
+  return (
+    <div>
+      <div className="tabwebdev">
+        <p>Website UI/UX</p>
+        <img src="/tab icon.png" alt="tab icon" />
+      </div>
+    </div>
+  );
 }
 
 export default Webdevelopment;
