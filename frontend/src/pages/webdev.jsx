12934e748fa08b25bf5d2cf6c71c75f82c1ae286
yashika <yashika0432@gmail.com>
2026-01-23 20:23:38 +0530

    aligning

--- frontend/src/pages/webdev.jsx
+++ frontend/src/pages/webdev.jsx
@@ -8,6 +8,22 @@ function Webdevelopment() {
         <img src="/tab icon.png" alt="tab icon" />
       </div>
       <h1 className="mainhead">My Projects</h1>
+      <div className="allprojects">
+        <div className="aaryaDes">
+          <div className="projectSerial">001.</div>
+          <div className="projectdetail aaryavartPj">
+            <img src="/aaryaPic.png" alt=""></img>
+            <p>
+              Aaryavart is a foundation for children with special needs and
+              having autism. This foundation provide different service for the
+              same. This website is to facilitate the the process of accessing
+              the same services and contains the color palette that do not
+              disturb the one having autism.<br></br> For better visuals click
+              the folder.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
