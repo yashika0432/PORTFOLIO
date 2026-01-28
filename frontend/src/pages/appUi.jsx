b09f53ca5ff3e6424aaf610bb3d288c4f27af0ef
yashika <yashika0432@gmail.com>
2026-01-28 21:50:31 +0530

    start webdev

--- frontend/src/pages/appUi.jsx
+++ frontend/src/pages/appUi.jsx
@@ -4,6 +4,7 @@ function AppDev() {
       <h1>Appdev work</h1>
       <div className="navbar"></div>
       <div className="mainpart"></div>
+      <div className="part2"></div>
 
       <div>app development works of ui ux</div>
     </div>
