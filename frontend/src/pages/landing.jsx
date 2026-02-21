4a70da4d14ea63ffbbba8653bb04bbff39ac0b90
yashika <yashika0432@gmail.com>
2026-02-21 20:16:08 +0530

    final static touches

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -13,7 +13,28 @@ export default function Landing() {
           <img src="/tab icon.png" alt="tab icon" />
         </div>
         <Navbar />
-        <div className="hero"></div>
+        <div className="hero">
+          <div className="usernames">
+            <div className="behance">
+              <a href="https://www.behance.net/yashikasharma68" target="_blank">
+                Yashikasharma68
+              </a>
+            </div>
+            <div className="insta">
+              <a href="https://www.instagram.com/yashika_.04/" target="_blank">
+                Yashika_.04
+              </a>
+            </div>
+            <div className="linkedin">
+              <a
+                href="https://www.linkedin.com/in/yashika-sharma-81110a291/"
+                target="_blank"
+              >
+                Yashika Sharma
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
       <Me />
       <Work />
